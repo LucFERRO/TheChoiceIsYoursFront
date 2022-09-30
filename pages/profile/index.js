cdf6184bc11624aa6e9c5d983c1a8da44e72f557
LucFERRO <luchenri.ferro@gmail.com>
2022-09-30 17:10:27 +0200

    profile page undefined next with ternary

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 
-export default function Profile({users}) {
+export default function Profile({users, loggedUser}) {
 
     const [loggedInUserId, setLoggedInUserId] = useState()
     const [currentUser, setCurrentUser] = useState()
@@ -15,12 +15,21 @@ export default function Profile({users}) {
     console.log(loggedInUserId)
     console.log(currentUser)
 
+    // console.log(loggedUser)
+
     // const user = users.find((user : userTypes) => user.username == req.body.username)
 
   return (
     <>
         <div className='container'>
             <h1>Profile</h1>
+            {/* <ul>
+                    <li>Username : {currentUser.username}</li>
+                    <li>Firstname: {currentUser.firstname}</li>
+                    <li>Lastname: {currentUser.lastname}</li>
+                    <li>Email: {currentUser.email}</li>
+                    <li>Date of birth: {currentUser.date_of_birth}</li>
+            </ul> */}
             {currentUser ?             
                 <ul>
                     <li>Username : {currentUser.username}</li>
@@ -39,9 +48,14 @@ export async function getStaticProps() {
     const res = await fetch('http://localhost:5000/api/users')
     const users = await res.json()
 
+    // const currentUserId = localStorage.getItem("currentUserId")
+    // const resUser = await fetch(`http://localhost:5000/api/users/${currentUserId}`)
+    // const loggedUser = await resUser.json()
+
     return {
         props: {
             users,
+            // loggedUser
         },
     }
 }
